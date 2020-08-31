178bc7bf75cbb0d7fd5e726c74c958036feae916
vinicius curti <vinicius_c_l@hotmail.com>
2020-08-31 18:59:34 -0300

    done controllers part

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,21 @@
 class CoursesController < ApplicationController
 
+  def new
+    @course = Course.new
+  end
+
+  def create
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to course_path(@course), notice: 'Course has been successfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:course).permit(:name, :category, :user_id, :description)
+  end
 end
