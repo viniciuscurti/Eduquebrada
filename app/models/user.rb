451d6f225e27425a30a8c5a9188081e9a25a8ddb
gabriel belo <gabrielrbelo@gmail.com>
2020-08-31 17:30:05 -0300

    teacher method created in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_many :courses
   has_many :enrollments
+
+
+  def teacher!
+    self.teacher = true
+    save
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
