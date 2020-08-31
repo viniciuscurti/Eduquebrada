d0ecb36140519c9d351bbdee2b1691ac5ff9c40a
rafael ferraz <rferraz_21@hotmail.com>
2020-08-31 19:21:32 -0300

    show_courses_ok

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,3 +1,13 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: :show
+  skip_before_action :authenticate_user!, only: :show
+ 
+  def show
+  end
+  
+  private
+  def set_course
+    @course = Course.find(params[:id])
+  end
 
 end
