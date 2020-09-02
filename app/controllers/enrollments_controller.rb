700d9824ada0bdb5a8bfec5e4c02b71b835594da
vinicius curti <vinicius_c_l@hotmail.com>
2020-09-02 20:18:15 -0300

    inicio de navbar

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,12 +1,11 @@
 class EnrollmentsController < ApplicationController
   def submit
-    send_data(generate_pdf, :filename => "DaUmaMAMADA.pdf", :type => "application/pdf") 
+    send_data(generate_pdf, :filename => "certificado.pdf", :type => "application/pdf") 
   end
   
   private 
   def generate_pdf
       Prawn::Document.new do
-        text "MAMA EU QUE O PAI BOTO O PDF PRA BAIXAR"
       end.render 
   end
 end
