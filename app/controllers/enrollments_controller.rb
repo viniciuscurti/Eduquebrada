bd8a7d18000ac70dd55effaae518f5eb9f3db342
gabriel belo <gabrielrbelo@gmail.com>
2020-09-09 11:20:29 -0300

    enrollment show and pdf background changed;

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -40,13 +40,14 @@ class EnrollmentsController < ApplicationController
     options = {
       page_size: "A5",
       page_layout: :landscape,
-      background: "app/assets/images/fundo_certificado.png",
+      background: "app/assets/images/fundo_certificado_2.jpeg",
       margin: [40, 75]
     }
     # enrollment = Enrollment.find(params[:id])
 
     Prawn::Document.new(options) do |pdf|
       pdf.fill_color "40464e"
+      pdf.move_down 20
       pdf.text "NOME DO CURSO", size: 40, style: :bold, align: :center
 
       pdf.move_down 30
@@ -58,13 +59,13 @@ class EnrollmentsController < ApplicationController
       pdf.move_down 15
       pdf.text "São Paulo, #{Time.now.strftime("%d/%m/%Y")}."
 
-      pdf.move_down 30
+      pdf.move_down 100
       # pdf.font Rails.root.join("fonts/custom.ttf")
-      pdf.text "howto", size: 24
+      # pdf.text "howto", size: 24
 
       pdf.move_up 5
       pdf.font "Helvetica"
-      pdf.text "https://eduquebrada.com.br/", size: 10
+      pdf.text "https://eduquebrada.com.br/", size: 15
     end.render
   end
 end
