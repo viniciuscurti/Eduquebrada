baf5aedfea97f80687a203180760bc56a494ea6b
vinicius curti <vinicius_c_l@hotmail.com>
2020-09-10 11:37:57 -0300

    carousel done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :construction ]
 
   def home
-    @last_courses = Course.order(:created_at).last(4)
+    @last_courses = Course.order(:created_at).last(8)
   end
 
   def construction
