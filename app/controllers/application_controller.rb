8ffee5e990b7ea9153844f055ddc72f47ea28db0
rafael ferraz <rferraz_21@hotmail.com>
2020-09-10 17:48:19 -0300

    page_error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,27 @@ class ApplicationController < ActionController::Base
     { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: :handle_exception
+  end
+  def not_found
+    raise ActionController::RoutingError.new('Not Found')
+  end
+  def handle_exception(exception=nil)
+    if exception
+      logger = Logger.new(STDOUT)
+      logger.debug "Exception Message: #{exception.message} \n"
+      logger.debug "Exception Class: #{exception.class} \n"
+      logger.debug "Exception Backtrace: \n"
+      logger.debug exception.backtrace.join("\n")
+      if [ActionController::RoutingError, ActionController::UnknownController, ActionController::UnknownAction].include?(exception.class)
+        return render_404
+      else
+        return render_500
+      end
+    end
+  end
+
   private
 
   def skip_pundit?
@@ -30,4 +51,20 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :occupation, :phone, :cpf, :teacher, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :occupation, :phone, :cpf, :teacher, :photo])
   end
+
+  
+  def render_404
+    respond_to do |format| 
+      format.html { render template: 'pages/home', layout: 'layouts/application', status: 404 } 
+      format.all { render nothing: true, status: 404 } 
+    end
+  end
+
+  def render_500
+    respond_to do |format|
+      format.html { render template: 'pages/home', layout: 'layouts/application', status: 500 }
+      format.all { render nothing: true, status: 500}
+    end
+  end
+
 end
